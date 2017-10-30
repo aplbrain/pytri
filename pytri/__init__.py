dff69d09ecb25062cb6dbb5913dadfb5f234a30f
Jordan Matelsky <j6k4m8@gmail.com>
2017-10-30 11:03:50 -0400

    [layers] Add FibersLayers

--- pytri/__init__.py
+++ pytri/__init__.py
@@ -221,60 +221,14 @@ class pytri:
         """
         if isinstance(data, np.ndarray):
             data = data.tolist()
-        _js = ("""
-        class FibersLayer extends Layer {
 
-            constructor(props) {
-                super(props);
-                this.data = props.data;
-                this.downsample = props.downsample || 1;
-                this.alpha = props.alpha || 1;
-            }
+        _js = ""
+        fibers_path, _ = split(__file__)
+        fibers_file = join(fibers_path, "js", "FibersLayer.js")
+        with open(fibers_file, 'r') as fh:
+            _js += ";\n\n" + fh.read().strip()
 
-            toggleVisible() {
-                this.children.forEach(c => {
-                    c.visible = !c.visible;
-                })
-            }
-
-            reload(fibers) {
-                let self = this;
-                self.children = [];
-                self.fibers = fibers;
-
-                for (var i = 0; i < self.fibers.length; i++) {
-                    var fiberGeometry = new window.THREE.Geometry();
-                    for (var j = 0; j < self.fibers[i].length; j++) {
-                        if (j % self.downsample === 0) {
-                            fiberGeometry.vertices.push(
-                                new window.THREE.Vector3(...self.fibers[i][j])
-                            );
-                        }
-                    }
-                    var line = new window.THREE.Line(
-                        fiberGeometry,
-                        new window.THREE.LineBasicMaterial({
-                            color: 0x00ffff,
-                            transparent: true,
-                            opacity: self.alpha
-                        })
-                    );
-                    self.children.push(line);
-
-                    self.scene.add(line);
-                }
-            }
-
-
-            requestInit(scene) {
-                let self = this;
-                self.scene = scene;
-                let fibers = self.data;
-                self.reload(fibers);
-            }
-        }
-
-        """ + """
+        _js += """
         V['"""+self.uid+"""'].addLayer('fibers', new FibersLayer({{
             data: {},
             colors: {},
@@ -284,5 +238,5 @@ class pytri:
             json.dumps(data),
             c,
             alpha
-        ))
+        )
         display(Javascript(_js))
