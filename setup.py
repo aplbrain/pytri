e5271a6efcb5e5a4ef9be82f2e93132292006a2b
Jordan Matelsky <j6k4m8@users.noreply.github.com>
2021-06-08 09:28:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="pytri",
-    version="2.0.0",
+    version="2.0.1",
     description="Pytri, redux",
     long_description=long_description,
     long_description_content_type="text/markdown",
